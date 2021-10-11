72778be590d7e61ca163ae9f2c1c1f2d0ea7b727
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-11 10:59:09 +0200

    add random and userchoice
    Adding functionality to the app. This commit I've added that the computer randomly picks between: Rock, Paper, Scissor.

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 window.onload = function () {
-  let inputSten = document.getElementById('sten')
-  let inputSaks = document.getElementById('saks')
-  let inputPapir = document.getElementById('papir')
+  let inputRock = document.getElementById('rock')
+  let inputPaper = document.getElementById('paper')
+  let inputScissor = document.getElementById('scissor')
   let computerChoice = document.getElementById('computerchoice')
-  console.log(inputSten)
-  console.log(inputSaks)
-  console.log(inputPapir)
 
-  let userChoice = 0
-  let computerChoiceRandom = Math.random()
-  console.log(computerChoice)
-
-  computerChoice.innerText = 'Computeren valgte: ' + computerChoiceRandom
+  getUserChoice()
+  getComputerChoice()
+  // result()
+  computerChoice.innerText = 'Computer choice: ' + getComputerChoice()
 }
+
+function getUserChoice(userChoice) {
+  document.getElementById('rock').onclick = function changeContent() {
+    console.log('ROCK')
+    return (userChoice = 'ROCK')
+  }
+
+  document.getElementById('paper').onclick = function changeContent() {
+    console.log('PAPER')
+    return (userChoice = 'PAPER')
+  }
+
+  document.getElementById('scissor').onclick = function changeContent() {
+    console.log('SCISSOR')
+    return (userChoice = 'SCISSOR')
+  }
+}
+
+function getComputerChoice(computerChoice) {
+  let computerChoiceRandom = Math.floor(Math.random() * 3)
+  if (computerChoiceRandom === 0) {
+    return (computerChoice = 'ROCK')
+  } else if (computerChoiceRandom === 1) {
+    return (computerChoice = 'PAPER')
+  } else {
+    return (computerChoice = 'SCISSOR')
+  }
+}
+
+// function result() {
+//   if (userChoice == 'ROCK' && computerChoice == 'SCISSOR') {
+//     console.log('You win')
+//   }
+// }
