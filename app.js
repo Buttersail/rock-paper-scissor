b7063e18f692c581fc83e0f868628bd160e58523
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-12 15:13:03 +0200

    add logic to result and refactor functions
    added the logic to the game, doesnt work yet though and refactored the functions with new knowledge

--- app.js
+++ app.js
@@ -1,45 +1,86 @@
 window.onload = function () {
-  let inputRock = document.getElementById('rock')
-  let inputPaper = document.getElementById('paper')
-  let inputScissor = document.getElementById('scissor')
-  let computerChoice = document.getElementById('computerchoice')
+  let inputRock = document.querySelector('#rock')
+  let inputPaper = document.querySelector('#paper')
+  let inputScissor = document.querySelector('#scissor')
+  let computerChoice = document.querySelector('#computerchoice')
 
-  getUserChoice()
-  getComputerChoice()
-  // result()
   computerChoice.innerText = 'Computer choice: ' + getComputerChoice()
+
+  console.log(result(getUserChoice(), getComputerChoice()))
 }
 
 function getUserChoice(userChoice) {
-  document.getElementById('rock').onclick = function changeContent() {
-    console.log('ROCK')
-    return (userChoice = 'ROCK')
+  document.querySelector('#rock').onclick = () => {
+    userChoice = 'ROCK'
+    return userChoice
   }
 
-  document.getElementById('paper').onclick = function changeContent() {
-    console.log('PAPER')
-    return (userChoice = 'PAPER')
+  document.querySelector('#paper').onclick = () => {
+    userChoice = 'PAPER'
+    return userChoice
   }
 
-  document.getElementById('scissor').onclick = function changeContent() {
-    console.log('SCISSOR')
-    return (userChoice = 'SCISSOR')
+  document.querySelector('#scissor').onclick = () => {
+    userChoice = 'SCISSOR'
+    return userChoice
+  }
+
+  document.querySelector('#result').onclick = () => {
+    console.log(userChoice)
+
+    result()
   }
 }
 
 function getComputerChoice(computerChoice) {
   let computerChoiceRandom = Math.floor(Math.random() * 3)
   if (computerChoiceRandom === 0) {
-    return (computerChoice = 'ROCK')
+    computerChoice = 'ROCK'
+    return computerChoice
   } else if (computerChoiceRandom === 1) {
-    return (computerChoice = 'PAPER')
+    computerChoice = 'PAPER'
+    return computerChoice
   } else {
-    return (computerChoice = 'SCISSOR')
+    computerChoice = 'SCISSOR'
+    return computerChoice
   }
 }
 
-// function result() {
-//   if (userChoice == 'ROCK' && computerChoice == 'SCISSOR') {
-//     console.log('You win')
-//   }
-// }
+function result(userChoice, computerChoice) {
+  //ROCK
+  if (userChoice == 'ROCK' && computerChoice == 'SCISSOR') {
+    console.log('You win. Rock beats scissor')
+    return 'You win. Rock beats scissor'
+  } else if (userChoice == 'ROCK' && computerChoice == 'PAPER') {
+    console.log('You loose. Paper beats rock')
+    return 'You loose. Paper beats rock'
+  } else if (userChoice == 'ROCK' && computerChoice == 'ROCK') {
+    console.log('Draw. You both choose rock')
+    return 'Draw. You both choose rock'
+  }
+  //SCISSOR
+  else if (userChoice == 'SCISSOR' && computerChoice == 'PAPAER') {
+    console.log('You win. Scissor beats paper')
+    return 'You win. Scissor beats paper'
+  } else if (userChoice == 'SCISSOR' && computerChoice == 'ROCK') {
+    console.log('You loose. Rock beats scissor')
+    return 'You loose. Rock beats scissor'
+  } else if (userChoice == 'SCISSOR' && computerChoice == 'SCISSOR') {
+    console.log('Draw. You both choose scissor')
+    return 'Draw. You both choose scissor'
+  }
+  //PAPER
+  else if (userChoice == 'PAPER' && computerChoice == 'ROCK') {
+    console.log('You win. Paper beats rock')
+    return 'You win. Paper beats rock'
+  } else if (userChoice == 'PAPER' && computerChoice == 'SCISSOR') {
+    console.log('You loose. Scissor beats paper')
+    return 'You loose. Scissor beats paper'
+  } else if (userChoice == 'PAPER' && computerChoice == 'PAPER') {
+    console.log('Draw. You both choose paper')
+    return 'Draw. You both choose paper'
+  } else {
+    console.log('ERROR')
+    return 'ERROR'
+  }
+}
